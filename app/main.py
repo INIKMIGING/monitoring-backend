ec153fcbb25b9a0a2ca220bc019ebae728a4f3fe
Muhamad Husnaya <muhamadhusnaya02@gmail.com>
2026-02-24 13:33:20 +0700

    add router modular and global health endpoint

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from app.api.v1.router import api_router
 
 app = FastAPI(
     title="Monitoring Backend API",
     version="0.1.0"
 )
 
+app.include_router(api_router, prefix="/api/v1")
+
 @app.get("/")
 def root():
     return {"message": "API is running"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
