0759ddf87bf2279820d9e597ddf8d5b57774dd37
Muhamad Husnaya <muhamadhusnaya02@gmail.com>
2026-02-27 11:13:14 +0700

    feat(migration): create initial monitoring time-series tables with indexing

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy.orm import sessionmaker, declarative_base # Tambahkan declarative_base
 from app.core.config import settings
+
+# 1. Definisikan Base di sini
+Base = declarative_base() 
 
 engine = create_engine(
     settings.database_url,
