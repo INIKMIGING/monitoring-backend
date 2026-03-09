8809d9beaf6057d84154b3e571c9c50bfbd6c31f
Muhamad Husnaya <muhamadhusnaya02@gmail.com>
2026-03-09 13:40:31 +0700

    feat(auth): fix problem cors request

--- app/main.py
+++ app/main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.router import api_router
 
 app = FastAPI(
     title="Monitoring Backend API",
     version="0.1.0"
 )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-app.include_router(api_router, prefix="/api/v1")
+app.include_router(api_router, prefix="/api")
 
-@app.get("/")
-def root():
-    return {"message": "API is running"}
+# @app.get("/")
+# def root():
+#     return {"message": "API is running"}
 
-@app.get("/health")
-def health_check():
-    return {"status": "healthy"}
+# @app.get("/health")
+# def health_check():
+#     return {"status": "healthy"}
